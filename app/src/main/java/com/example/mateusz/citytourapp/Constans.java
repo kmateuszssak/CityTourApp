4682f7fe69fab7391f57591083408555abf3bdc2
Your Name <you@example.com>
2017-12-09 20:47:06 +0100

    Dodanie apiKeyOrange.

--- app/src/main/java/com/example/mateusz/citytourapp/Constans.java
+++ app/src/main/java/com/example/mateusz/citytourapp/Constans.java
@@ -6,5 +6,6 @@ package com.example.mateusz.citytourapp;
 
 public class Constans {
 
-    public static final String apiKey = "68b8fa39-7cf6-428e-b32c-8144b601e603";
+    public static final String apiKeyGoogle = "68b8fa39-7cf6-428e-b32c-8144b601e603";
+    public static final String apiKeyOrange = "rleprPWnQZRXtGO9uvzAZoIOPIUGZ77L";
 }
